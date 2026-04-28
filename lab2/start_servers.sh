c141889044bb82f99f39e5b236d1f271dbebb4a6
Idashxdx <mashrabova2003@bk.ru>
2026-04-29 01:53:51 +0300

    задание 2 правка

--- lab2/start_servers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo cp lab2/nginx.conf /etc/nginx/sites-available/lab2
-sudo ln -s /etc/nginx/sites-available/lab2 /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo systemctl restart nginx
-
-gunicorn --workers 4 --bind 127.0.0.1:8000 lab2.wsgi_app:application
--- lab2/test_performance.sh
+++ lab2/test_performance.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+echo -e "\nPicture"
+curl -I http://localhost:8080/public/test.jpg
+echo "Password"
+curl http://localhost:8080/gunicorn
+echo -e "\nDirect access"
+curl http://127.0.0.1:8000/
 
-echo "Testing public location..."
-ab -n 1000 -c 10 http://localhost:8080/public/test.jpg
+echo -e "\n\nTesting public location..."
+wrk -t8 -c972 -d20s http://127.0.0.1:8080/public/test.jpg
+sleep 3
+echo -e "\n+10%"
+wrk -t8 -c1069 -d20s http://127.0.0.1:8080/public/test.jpg
+sleep 3
+echo -e "\n\nTesting gunicorn location..."
+wrk -t8 -c150 -d15s http://127.0.0.1:8080/gunicorn
+sleep 3
+echo -e "\n+10%"
+wrk -t8 -c165 -d15s http://127.0.0.1:8080/gunicorn
+sleep 3
+echo -e "\n\nTesting WSGI directly..."
+wrk -t8 -c150 -d15s http://127.0.0.1:8000/
+sleep 3
+echo -e "\n+10%"
+wrk -t8 -c165 -d15s http://127.0.0.1:8000/
 
-echo "Testing gunicorn location..."
-ab -n 1000 -c 10 http://localhost:8080/gunicorn
-
-echo "Testing WSGI directly..."
-ab -n 1000 -c 10 http://localhost:8000/
